0f32029a232c19b0aba2f292ce42bd5b19744707
Yash Mahesh Bangera <yashmaheshbangera.work@gmail.com>
2026-04-30 06:50:02 -0700

    Direct model invocation

---
+++ model/direct_model_invoke.py
@@ -0,0 +1,19 @@
+import os
+from langchain.chat_models import init_chat_model
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+def test_chat_model():
+
+    model = init_chat_model(
+        "microsoft/Phi-3-mini-4k-instruct",
+        model_provider="huggingface",
+        temperature=0.7,
+        max_tokens=1024,
+    )
+    response = model.invoke("How do I create a local server?")
+    print(response)
+    return None
+
+if __name__ == "__main__":
+    test_chat_model()
